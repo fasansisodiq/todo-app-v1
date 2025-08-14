5dc7a028ba6cbb792b36c07c6e1ae38550c82e28
Fasansi sodiq <132054522+fasansisodiq@users.noreply.github.com>
2025-08-14 11:22:27 +0100

    Update ProfileDesign.jsx

--- src/pages/navBarPages/menu/ProfilePage/profile/ProfileDesign.jsx
+++ src/pages/navBarPages/menu/ProfilePage/profile/ProfileDesign.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProfileDesign({ children, bg }) {
   return (
     <div
-      className={`flex flex-col justify-start gap-1 mt-2 sm:mt-4 py-2 sm:py-8 w-full sm:w-1/2 h-fit  rounded-lg shadow-md dark:shadow-lg p-4 pl-4  sm:pl-8 lg:pl-15 lg:text-2xl text-lg sm:text-xl text-slate-600 ${bg}`}
+      className={`flex flex-col justify-start gap-1 mt-2 sm:mt-4 py-2 sm:py-8 w-screen sm:w-1/2 h-fit  rounded-lg shadow-md dark:shadow-lg p-4 pl-4  sm:pl-8 lg:pl-15 lg:text-2xl text-lg sm:text-xl text-slate-600 ${bg}`}
     >
       {children}
     </div>
