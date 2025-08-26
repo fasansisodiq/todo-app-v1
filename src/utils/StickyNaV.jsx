1fd8456c3783d184e79b0da97c917d5469b5e440
Fasansi sodiq <132054522+fasansisodiq@users.noreply.github.com>
2025-08-26 11:23:15 +0100

    Update StickyNaV.jsx

--- src/utils/StickyNaV.jsx
+++ src/utils/StickyNaV.jsx
@@ -3,3 +3,5 @@ function StickyNav ({children}) => {
    <nav className="w-full h-12 bg-emerald-50 sm:h-15 md:h-24  shadow-2xl fixed top-0 right-0 left-0 z-50 justify-between px-4  sm:px-6 md:px-8 flex  items-center">{children}</nav>
 );
 }
+
+export default StickyNav 
