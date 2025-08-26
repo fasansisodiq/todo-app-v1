a9d25961bd3ec240bc2915b22bd8b74fa0e8b955
Fasansi sodiq <132054522+fasansisodiq@users.noreply.github.com>
2025-08-26 11:30:13 +0100

    Update StickyNaV.jsx

--- src/utils/StickyNaV.jsx
+++ src/utils/StickyNaV.jsx
@@ -1,7 +1,8 @@
 function StickyNav ({children}) => {
  return (
-   <nav className="w-full h-12 bg-emerald-50 sm:h-15 md:h-24  shadow-2xl fixed top-0 right-0 left-0 z-50 justify-between px-4  sm:px-6 md:px-8 flex  items-center">{children}</nav>
+   <nav className="w-full h-12 border-b  transition-colors duration-300 bg-white/80 dark:bg-[#232b25]/80 border-emerald-100 dark:border-emerald-900 sm:h-15 md:h-24  shadow-2xl fixed top-0 right-0 left-0 z-50 justify-between px-4  sm:px-6 md:px-8 flex  items-center">{children}</nav>
 );
 }
 
 export default StickyNav;
+ 
