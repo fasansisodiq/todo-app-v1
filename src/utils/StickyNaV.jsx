3d0b26a1cb19ecc381d3712d79e0b860fc7d3e36
Fasansi sodiq <132054522+fasansisodiq@users.noreply.github.com>
2025-09-14 14:24:07 +0100

    Update StickyNaV.jsx

--- src/utils/StickyNaV.jsx
+++ src/utils/StickyNaV.jsx
@@ -5,7 +5,7 @@ function StickyNav({ children }) {
         border-b gap-4  md:gap-8 lg:gap-10 
         
         h-14  lg:px-12
-        rounded-b-2xl backdrop-blur-md
+         backdrop-blur-md
         duration-300 text-[0.7rem] sm:text-[0.9rem]  transition-colors  bg-white/80 dark:bg-[#232b25]/80 border-emerald-100 dark:border-emerald-900 sm:h-15 md:h-24  shadow-2xl fixed top-0 right-0 left-0 z-50 justify-end px-4  sm:px-6 md:px-8 flex  items-center"
     >
       {children}
