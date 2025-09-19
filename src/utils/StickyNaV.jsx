29ace9e9e50f5cd4eb9e4120df56cc60a38523fd
Fasansi sodiq <132054522+fasansisodiq@users.noreply.github.com>
2025-09-19 10:30:15 +0100

    Update StickyNaV.jsx

--- src/utils/StickyNaV.jsx
+++ src/utils/StickyNaV.jsx
@@ -2,7 +2,7 @@ function StickyNav({ children }) {
   return (
     <nav
       className="w-full   bg-gradient-to-r from-emerald-50 via-white to-emerald-100 dark:from-[#232b25] dark:via-[#181f1b] dark:to-[#232b25]
-        border-b gap-4  md:gap-8 lg:gap-10 
+        border-b gap-6  md:gap-8 lg:gap-10 
         
         h-14  lg:px-12
          backdrop-blur-md
