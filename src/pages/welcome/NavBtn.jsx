63e18eba39cbffa81b405e8462832c4f702e31c1
Fasansi sodiq <132054522+fasansisodiq@users.noreply.github.com>
2025-09-19 10:24:31 +0100

    Update NavBtn.jsx

--- src/pages/welcome/NavBtn.jsx
+++ src/pages/welcome/NavBtn.jsx
@@ -10,7 +10,7 @@ const NavBtn = ({ to, label, onClick }) => {
       <button
         type="button"
         onClick={onClick}
-        className={`px-6 py-2 rounded-2xl font-semibold shadow-md hover:shadow-xl hover:scale-105 focus:outline-none focus:ring-2 transition-all duration-200 text-sm sm:text-xl flex items-center justify-center gap-2
+        className={`px-2 py-1 sm:px-6 sm:py-2 rounded-2xl font-semibold shadow-md hover:shadow-xl hover:scale-105 focus:outline-none focus:ring-2 transition-all duration-200 text-sm sm:text-xl flex items-center justify-center gap-2
           ${
             label === "Login"
               ? "bg-emerald-50 text-emerald-700 border border-emerald-300 focus:ring-emerald-200"
