6cb7f328fc29ef528ebe194e8dc35585fce028eb
Fasansi sodiq <132054522+fasansisodiq@users.noreply.github.com>
2025-09-23 12:38:19 +0100

    Update Logout.jsx

--- src/ui/navBarUI/Logout.jsx
+++ src/ui/navBarUI/Logout.jsx
@@ -1,17 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../authentication/useAuth";
 import NavBtn from "../../pages/welcome/NavBtn";
-
+import { useDarkMode } from "../../customHooks/DarkModeContext"; // import the hook
 
 function Logout() {
   const navigate = useNavigate();
-//const {toggleDarkMode} = useDarkMode();
   const { logOut } = useAuth();
+  const { setDarkMode } = useDarkMode(); // get setDarkMode
+
   // Function to sign out the current user
   const handleLogOut = () => {
     logOut();
+    setDarkMode(false); // reset to light mode
     navigate("/login");
-//toggleDarkMode();
   };
 
   return (
